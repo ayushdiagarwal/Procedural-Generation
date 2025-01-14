7ebed344b811b84e7f4c95786bcbbd9eeced29e2
Ayush Agarwal <ayushdiagarwal@gmail.com>
2025-01-15 00:42:32 +0530

    Refactored code

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-var canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth - 50;
-canvas.height = 250;
+var rcanvas = document.querySelector("#random")
+rcanvas.width = window.innerWidth - 50;
+rcanvas.height = 250;
 
-var ctx = canvas.getContext('2d');
+var pcanvas = document.querySelector("#perlin");
+pcanvas.width = window.innerWidth - 50;
+pcanvas.height = 250;
+
+var pctx = pcanvas.getContext('2d');
+var rctx = rcanvas.getContext('2d');
 
 class PsuedoRandom {
     constructor(seed) {
@@ -30,29 +35,23 @@ class PsuedoRandom {
 }
 
 class Dot {
-    constructor(x, y, color="blue", size="4") {
+    constructor(x, y, ctx,color="blue", size="4") {
         this.x = x;
         this.y = y;
         this.color = color;
         this.size = size;
+        this.ctx = ctx;
     }
 
     drawDot() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.size, this.size);
-    }
-
-    getX() {
-        return this.x;
-    }
-
-    getY() {
-        return this.y;
+        this.ctx.fillStyle = this.color;
+        this.ctx.fillRect(this.x, this.y, this.size, this.size);
     }
 }
 
-function smoothstep(a,b,t) {
-    return t * t * (b -a * t); // Cubic smoothing
+
+function lerp(a, b, t) {
+    return a + t * (b - a); // t is a value between 0 and 1
 }
 
 function cosineInterpolation(a,b,t) {
@@ -62,22 +61,23 @@ function cosineInterpolation(a,b,t) {
 
 
 class RandPop {
-    constructor(size) {
+    constructor(size, ctx) {
         this.dots = [];
+        this.ctx = ctx;
         let posX = 10;
         const rng1 = new PsuedoRandom(Date.now());
-        let positionsY = rng1.randArray(size, 1, canvas.height);
+        let positionsY = rng1.randArray(size, 1, rcanvas.height);
 
         // Random line generation
         for(let i=0; i<size; i++) {
-            this.dots[i] = new Dot(posX, positionsY[i]);
+            this.dots[i] = new Dot(posX, positionsY[i], this.ctx);
         
             // draw the path
-            ctx.beginPath();
-            ctx.moveTo(posX, positionsY[i]);
+            this.ctx.beginPath();
+            this.ctx.moveTo(posX, positionsY[i]);
             posX += 50;
-            ctx.bezierCurveTo(posX, positionsY[i+1]);
-            ctx.stroke();
+            this.ctx.bezierCurveTo(posX, positionsY[i+1]);
+            this.ctx.stroke();
 
             this.dots[i].drawDot();
         }
@@ -85,18 +85,19 @@ class RandPop {
 }
 
 class RandSmoothPopCosine {
-    constructor(size) {
+    constructor(size, ctx) {
 
         this.dots = [];
+        this.ctx = ctx;
         let posX = 10;
 
         // Generate random points
         const rng1 = new PsuedoRandom(Date.now());
-        let positionsY = rng1.randArray(size, 1, canvas.height);
+        let positionsY = rng1.randArray(size, 1, rcanvas.height);
 
         for(let i=0; i<size; i++) {
-            this.dots.push(new Dot(posX, positionsY[i]));
-            posX += 50;
+            this.dots.push(new Dot(posX, positionsY[i], this.ctx));
+            posX += rcanvas.width/size;
 
             this.dots[i].drawDot();
         }
@@ -123,90 +124,25 @@ class RandSmoothPopCosine {
     }
 
     drawSmoothCurve() {
-        ctx.beginPath();
-        ctx.moveTo(this.smoothstep[0].x, this.smoothstep[0].y);
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.smoothstep[0].x, this.smoothstep[0].y);
 
         for (let i = 1; i < this.smoothstep.length; i++) {
-            ctx.lineTo(this.smoothstep[i].x, this.smoothstep[i].y);
+            this.ctx.lineTo(this.smoothstep[i].x, this.smoothstep[i].y);
         }
 
-        ctx.strokeStyle = "purple";
-        ctx.stroke();
-    }
-}
-
-class RandSmoothPop {
-    constructor(size) {
-        this.dots = [];
-        let posX = 10;
-        const rng1 = new PsuedoRandom(Date.now());
-        let positionsY = rng1.randArray(size, 1, canvas.height);
-
-        // Draw smoothed line
-        ctx.beginPath();
-        ctx.moveTo(posX, positionsY[0]); // Start at the first point
-
-        for (let i = 1; i < size - 1; i++) {
-            const x1 = posX;
-            const y1 = positionsY[i - 1];
-            const x2 = posX + 50;
-            const y2 = positionsY[i];
-            const x3 = posX + 100;
-            const y3 = positionsY[i + 1];
-
-            // Control points for cubic Bézier
-            const cp1x = x1 + 25; // Midpoint between x1 and x2
-            const cp1y = y1;
-            const cp2x = x2 - 25; // Midpoint between x2 and x3
-            const cp2y = y2;
-
-            ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x2, y2);
-
-            posX += 50;
-        }
-
-        ctx.stroke();
-
-        // Draw dots
-        posX = 10;
-        for (let i = 0; i < size; i++) {
-            this.dots[i] = new Dot(posX, positionsY[i]);
-            this.dots[i].drawDot();
-            posX += 50;
-        }
-    }
-}
-
-
-function hash(x) {
-    return Math.sin(x * 127.1) * 43758.5453 % 1; // Random-like output
-}
-
-
-function lerp(a, b, t) {
-    return a + t * (b - a); // t is a value between 0 and 1
-}
-
-
-
-
-class Pop {
-    constructor(size) {
-        this.dots = [];
-        this.posX = 10;
-
-        for (let i=0; i<size; i++) {
-            this.dots[i] = new Dot(posX, )
-        }
-        
+        this.ctx.strokeStyle = "purple";
+        this.ctx.stroke();
     }
 }
 
 function gameLoop() {
     // clear screen
-    // ctx.clearRect(0,0, canvas.width, canvas.height)
+    // rctx.clearRect(0,0, rcanvas.width, rcanvas.height)
     // draw population
-    const popy = new RandSmoothPopCosine(50);
+    const popy = new RandSmoothPopCosine(20, rctx);
+    const popi = new RandSmoothPopCosine(100, pctx);
+
 
     // requestAnimationFrame(gameLoop);
 
