521c475a7a259a492d7b19bcc42bfe77a2c80416
Ayush Agarwal <ayushdiagarwal@gmail.com>
2025-01-16 01:52:53 +0530

    Added 1d Perlin Noise

--- appy.js
+++ appy.js
@@ -14,7 +14,6 @@ class PsuedoRandom {
         this.seed = seed;
     }
 
-    // Generates a random number between 0 and 1
     random() {
         const x = Math.sin(this.seed++)*10000;
         return x - Math.floor(x); 
@@ -28,45 +27,44 @@ class PsuedoRandom {
         return this.random() * (max-min) + min;
     }
 
-    // Generate an array of n random numbers
     randArray(n, min, max) {
         return Array(n).fill(0).map(() => this.randFloat(min, max));
     }
 }
 
 class PerlinNoise {
-
     constructor(seed) {
         this.rng = new PsuedoRandom(seed);
         this.gradients = [];
     }
 
     generateGradients(size) {
-        this.gradients = this.rng.randArray(size, -1, 1); // generate random gradient value between 0 and 1
+        this.gradients = this.rng.randArray(size, -1, 1);
     }
 
-    // This function is used to ease/smooth, I think we can also use cosine interpolation instead of this one
     fade(t) {
         return t * t * t * (t * (t * 6 - 15) + 10); // Quintic interpolation
     }
 
     noise(x) {
-         const x0 = Math.floor(x);
-         const x1 = x0+1;
-         const dx = x - x0;
-         
-         const fx = this.fade(dx);
+        const x0 = Math.floor(x);
+        const x1 = x0 + 1;
+        const dx = x - x0;
 
-         const g0 = this.gradients[x0 % this.gradients.length] * dx;
-         const g1 = this.gradients[x1 % this.gradients.length] * (dx - 1); // dx - 1 || 1 - d
+        const fx = this.fade(dx);
+        
+        const g0 = this.gradients[x0 % this.gradients.length];
+        const g1 = this.gradients[x1 % this.gradients.length];
+        
+        const n0 = g0 * dx;
+        const n1 = g1 * (dx - 1);
 
-        return lerp(g0, g1, fx);
-
+        return lerp(n0, n1, fx);
     }
 }
 
 class Dot {
-    constructor(x, y, ctx,color="blue", size="4") {
+    constructor(x, y, ctx, color="blue", size="4") {
         this.x = x;
         this.y = y;
         this.color = color;
@@ -80,9 +78,8 @@ class Dot {
     }
 }
 
-
 function lerp(a, b, t) {
-    return a + t * (b - a); // t is a value between 0 and 1
+    return a + t * (b - a);
 }
 
 function cosineInterpolation(a,b,t) {
@@ -90,6 +87,48 @@ function cosineInterpolation(a,b,t) {
     return a + tRemapped * (b-a);
 }
 
+class PerlinNoiseVisualizer {
+    constructor(size, ctx, frequency = 0.05) {
+        this.perlin = new PerlinNoise(Date.now());
+        this.perlin.generateGradients(size);
+        this.ctx = ctx;
+        this.frequency = frequency;
+        this.dots = [];
+        
+        // Generate and draw the noise
+        let posX = 10;
+        const spacing = ctx.canvas.width / size;
+        
+        // Generate points using Perlin noise
+        for(let i = 0; i < size; i++) {
+            const noiseVal = this.perlin.noise(i * this.frequency);
+            // Map noise value from [-1,1] to canvas height
+            const posY = ((noiseVal + 1) / 2) * ctx.canvas.height;
+            
+            this.dots.push(new Dot(posX, posY, this.ctx, "green"));
+            posX += spacing;
+        }
+        
+        // Draw dots and connect them
+        this.drawNoiseCurve();
+    }
+    
+    drawNoiseCurve() {
+        // Draw connecting line
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.dots[0].x, this.dots[0].y);
+        
+        for(let i = 1; i < this.dots.length; i++) {
+            this.ctx.lineTo(this.dots[i].x, this.dots[i].y);
+        }
+        
+        this.ctx.strokeStyle = "green";
+        this.ctx.stroke();
+        
+        // Draw dots
+        this.dots.forEach(dot => dot.drawDot());
+    }
+}
 
 class RandPop {
     constructor(size, ctx) {
@@ -166,55 +205,19 @@ class RandSmoothPopCosine {
         this.ctx.stroke();
     }
 }
-
-class PerlinNoiseVisualizer{
-
-    constructor(size,ctx,freq = 0.05) {
-        this.dots = [];
-        this.perlin = new PerlinNoise(Date.now());
-        this.perlin.generateGradients(size);
-        this.ctx = ctx;
-        this.freq= freq;
-        let posX = 10;
-        
-        const spacing = ctx.canvas.width/size;
-        for(let i = 0; i < size; i++) {
-            const noiseVal = this.perlin.noise(i * this.freq);
-            // whyy?
-            const posY = ((noiseVal + 1) / 2) * this.ctx.canvas.height;
-            
-            this.dots.push(new Dot(posX, posY, this.ctx, "green"));
-            posX += spacing;
-        }
-        // Draw dots and connect them
-        this.drawNoiseCurve();
-    }
-    
-    drawNoiseCurve() {
-        this.ctx.beginPath();
-        this.ctx.moveTo(this.dots[0].x, this.dots[0].y);
-        
-        for(let i = 1; i < this.dots.length; i++) {
-            this.ctx.lineTo(this.dots[i].x, this.dots[i].y);
-        }
-        
-        this.ctx.strokeStyle = "green";
-        this.ctx.stroke();
-        
-        this.dots.forEach(dot => dot.drawDot());
-    }
-}
-
 function gameLoop() {
-    // clear screen
-    // rctx.clearRect(0,0, rcanvas.width, rcanvas.height)
-    // draw population
+    // Clear both canvases
+    rctx.clearRect(0, 0, rcanvas.width, rcanvas.height);
+    pctx.clearRect(0, 0, pcanvas.width, pcanvas.height);
+    
+    // Draw random smooth population on first canvas
     const popy = new RandSmoothPopCosine(30, rctx);
-    const Ppop = new PerlinNoiseVisualizer(50, pctx);
-
-
+    
+    // Draw Perlin noise on second canvas
+    const perlinViz = new PerlinNoiseVisualizer(100, pctx);
+    
+    // Uncomment to animate
     // requestAnimationFrame(gameLoop);
-
 }
 
-gameLoop()
+gameLoop();
